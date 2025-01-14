1522e02b2af6e37ad825da810123c7a3f0f39a01
bwieckow <bar.wieckowski@gmail.com>
2025-01-14 16:14:28 +0100

    Initial commit; Terraform pipelines; Lambda terraform code; Basic python function

---
+++ infrastructure/main.tf
@@ -0,0 +1,19 @@
+resource "aws_lambda_function" "python_lambda" {
+  function_name = "my_python_lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.13" # Latest Python version supported by AWS Lambda
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+output "lambda_function_arn" {
+  value = aws_lambda_function.python_lambda.arn
+}
