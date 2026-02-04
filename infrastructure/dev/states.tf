49261da2d5a469efb8672550a2f8c7028a8a9109
bwieckow <bartlomiej.wieckowski@cieclekeurope.com>
2026-02-04 14:20:06 +0100

    Update docs and delete profile

--- infrastructure/dev/states.tf
+++ infrastructure/dev/states.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "codebazar-states"
-    region  = "eu-west-1"
-    key     = "states/opsmasters/calendar/terraform-dev.tfstate"
-    profile = "codebazar"
+    bucket = "codebazar-states"
+    region = "eu-west-1"
+    key    = "states/opsmasters/calendar/terraform-dev.tfstate"
     # dynamodb_table = "terraform-state-management"
   }
 }
